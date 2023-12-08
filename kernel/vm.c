cf2f1618f5a467512b2655001fb3d183fb77f7a7
xiunian <nniferyy@gmail.com>
2023-12-08 17:58:21 +0000

    fix syscall params lazy problem

--- kernel/vm.c
+++ kernel/vm.c
@@ -102,32 +102,10 @@ walkaddr(pagetable_t pagetable, uint64 va)
   if(va >= MAXVA)
     return 0;
 
-  struct proc* p = myproc();
   pte = walk(pagetable, va, 0);
-  if(pte == 0 || (*pte & PTE_V) == 0){
-    // 缺页中断
-    if(va >= p->sz ||  va < PGROUNDUP(p->trapframe->sp) ||PGROUNDDOWN(va) >= MAXVA){
-      //printf("va=%p stack=%p\n",va,PGROUNDUP(r_sp()));
-      p->killed = 1;
-      exit(-1);
-    } else{
-      char* mem = kalloc();
-      if(mem != 0){
-        memset(mem, 0, PGSIZE);
-        //printf("trap: %d %p\n",p->pid,PGROUNDDOWN(va));
-        if(mappages(p->pagetable,PGROUNDDOWN(va), PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
-          kfree(mem);
-          p->killed = 1;
-          exit(-1);
-        }
-      } else {
-        p->killed = 1;
-        exit(-1);
-      }
-    }
-  }
-  else if((*pte & PTE_U) == 0)
+  if(pte == 0 || (*pte & PTE_V) == 0 || (*pte & PTE_U) == 0){
     return 0;
+  }
   pa = PTE2PA(*pte);
   return pa;
 }
