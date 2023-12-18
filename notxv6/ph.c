d3ee8314c69503d48935d34d29e0a2097e538b25
xiunian <nniferyy@gmail.com>
2023-12-18 09:35:07 +0000

    fix task2

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -44,6 +44,7 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  pthread_mutex_lock(&locks[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -53,10 +54,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
-    pthread_mutex_lock(&locks[i]);
     insert(key, value, &table[i], table[i]);
-    pthread_mutex_unlock(&locks[i]);
   }
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
